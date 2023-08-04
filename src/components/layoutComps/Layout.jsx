1d7c8a025d864ac1c106ed8771ab19086967bfa1
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2023-08-04 22:47:38 +0530

    Added animation

--- src/components/layoutComps/Layout.jsx
+++ src/components/layoutComps/Layout.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import { motion } from "framer-motion";
 
 const Layout = ({ children }) => {
   return (
-    <div className="mx-auto container max-w-7xl px-4">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3, delay: 0 }}
+      className="mx-auto container max-w-7xl px-4"
+    >
       <Navbar />
       {children}
       <Footer />
-    </div>
+    </motion.div>
   );
 };
 
